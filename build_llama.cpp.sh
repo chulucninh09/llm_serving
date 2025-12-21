c26db83aedae5382bc431d1eb938aa2ffdadb44b
Ninh Chu <chulucninh09@gmail.com>
2025-12-21 21:03:35 +0000

    isolate cpu when build llama cpp

--- build_llama.cpp.sh
+++ build_llama.cpp.sh
@@ -1,2 +1,2 @@
 cmake -B build -DGGML_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES="86" -DLLAMA_CURL=OFF -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=FLAME
-cmake --build build --config Release -j 16
+taskset -c 0-15 cmake --build build --config Release -j 16
