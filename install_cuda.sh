1f50e68a19d63c19d2d3b7f9f3d4c281b72afe69
Ninh Chu <chulucninh09@gmail.com>
2025-12-21 17:38:35 +0000

    add config and guide

--- install_cuda.sh
+++ install_cuda.sh
@@ -2,3 +2,8 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-13-1
+
+# Add cuda to path
+echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
