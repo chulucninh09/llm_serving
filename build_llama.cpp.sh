d01f1ffe0bb2ff0f74480fabd361e9cb33759118
Ninh Chu <chulucninh09@gmail.com>
2025-12-21 17:40:53 +0000

    add more config

--- build_llama.cpp.sh
+++ build_llama.cpp.sh
@@ -1,2 +1,2 @@
-cmake -B build -DGGML_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES="86"
+cmake -B build -DGGML_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES="86" -DLLAMA_CURL=OFF
 cmake --build build --config Release -j 8
